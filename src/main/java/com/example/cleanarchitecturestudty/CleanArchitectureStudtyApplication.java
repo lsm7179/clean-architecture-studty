f2961f1aec963b9b84e3c0704c5b7099ed62306e
leeseungmin2 <lsm7179@hanssem.com>
2024-02-01 15:30:06 +0900

    chore: spring boot 초기 세팅

---
+++ src/main/java/com/example/cleanarchitecturestudty/CleanArchitectureStudtyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturestudty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureStudtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureStudtyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cleanarchitecturestudty/CleanArchitectureStudtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturestudty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureStudtyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
